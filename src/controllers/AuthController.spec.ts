c081f8424634793f0f8becb738fefafe51f1a818
m-milovanovic <m.milovanovic@outlook.com>
2021-06-17 18:06:20 +0200

    change tests

--- src/controllers/AuthController.spec.ts
+++ src/controllers/AuthController.spec.ts
@@ -7,13 +7,14 @@ import { Role } from '../helpers/shared';
 
 const app = createServer();
 
-describe('test AuthController', () => {
-  const spy = jest
-    .spyOn(UserService, 'findByUsername')
-    .mockResolvedValueOnce(
-      new User({ username: 'user', password: bcrypt.hashSync('user', 10), role: Role.User })
-    );
+const spy = jest
+  .spyOn(UserService, 'findByUsername')
+  .mockResolvedValueOnce(
+    new User({ username: 'user', password: bcrypt.hashSync('user', 10), role: Role.User })
+  )
+  .mockResolvedValue(null);
 
+describe('test AuthController', () => {
   it('successfully logs in', async () => {
     const response = await supertest(app).post('/api/auth/login').send({
       username: 'user',
@@ -32,5 +33,5 @@ describe('test AuthController', () => {
     expect(spy).toBeCalled();
     expect(spy).toHaveBeenCalledWith('nonuser');
     expect(response.status).toBe(400);
-  });
+  }, 15000);
 });
