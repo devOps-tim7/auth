c0db1dc32202509ce8c89588e55fb05b92b1db21
Marko131 <markostanic222@gmail.com>
2021-06-20 10:23:58 +0200

    Add metrics for prometheus

--- server.ts
+++ server.ts
@@ -5,11 +5,15 @@ import express from 'express';
 import cors from 'cors';
 import errorHandler from './src/middleware/ErrorHandler';
 import AuthRoute from './src/routes/Auth';
+import metricsMiddleware from './src/middleware/MetricsMiddleware';
+import MetricsRoutes from './src/routes/Metrics';
 
 export const createServer = () => {
   const app = express();
   app.use(express.json({ limit: '8mb' }));
   app.use(cors());
+  app.use(metricsMiddleware);
+  app.use('/metrics', MetricsRoutes);
   app.use('/api/auth', AuthRoute);
   app.use(errorHandler);
   return app;
