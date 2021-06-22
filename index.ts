1bb3d70e74dbad54dfbe37b20905118311b2999c
Marko131 <markostanic222@gmail.com>
2021-06-22 18:47:20 +0200

    Calculate traffic in megabytes and add metrics

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import { createServer } from './server';
-
+import RequestStatsMiddleware from './src/middleware/RequestStatsMiddleware';
 const app = createServer();
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server has started at http://localhost:${port}`);
 });
+
+RequestStatsMiddleware(server)    
+
